ba2e6992cc5a79e30700425a432cd8cc37e16ae7
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-26 17:31:36 +0100

    Sistemata la sintassi dello script per generare le tabelle in sql.

--- prototype/sql/create.sql
+++ prototype/sql/create.sql
@@ -1,46 +1,47 @@
 -- queries in sql per creare le tabelle
 
 -- crezione tabella project
-CREATE TABLE `db_app_techweb`.`project` (
-    `id` INT NOT NULL AUTO_INCREMENT , 
-    `nome` VARCHAR(255) NOT NULL , 
-    `descrizione` TEXT NOT NULL , 
-    PRIMARY KEY (`id`));
+CREATE TABLE project (
+    id INT NOT NULL AUTO_INCREMENT , 
+    nome VARCHAR(255) NOT NULL , 
+    descrizione TEXT NOT NULL , 
+    PRIMARY KEY (id));
 
 -- creazione tabella user
-CREATE TABLE `db_app_techweb`.`user` (
-    `email` VARCHAR(255) NOT NULL , 
-    `username` VARCHAR(255) NOT NULL , 
-    `password` VARCHAR(255) NOT NULL,
-     PRIMARY KEY (`email`));
+CREATE TABLE user (
+    email VARCHAR(255) NOT NULL , 
+    username VARCHAR(255) NOT NULL , 
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (email)
+);
 
 -- creazione della tabella movimento 
 -- spesa non sembra essere un nome adatto in quanto implica un
 -- importo sempre negativo
-CREATE TABLE `db_app_techweb`.`movimento` (
-    `id_project` INT NOT NULL , 
-    `data` DATETIME NOT NULL , 
-    `importo` FLOAT NOT NULL , 
-    `descrizione` TEXT, 
-    PRIMARY KEY (`id_project`, `data`),
-    FOREIGN KEY (`id_project`) REFERENCES (`project`.`id`),
+CREATE TABLE movimento (
+    id_project INT NOT NULL , 
+    data DATETIME NOT NULL , 
+    importo FLOAT NOT NULL , 
+    descrizione TEXT, 
+    PRIMARY KEY (id_project, data),
+    FOREIGN KEY (id_project) REFERENCES (project.id),
 );
 
 -- creazione della relazione tag
-CREATE TABLE `db_app_techweb`.`tag` (
-    `nome` VARCHAR(255) NOT NULL, 
-    `id_project` INT NOT NULL,
-    `descrizione` TEXT , 
-    PRIMARY KEY (`nome`, `id_project`),
-    FOREIGN KEY (`id_project`) REFERENCES (`project`.`id`)
+CREATE TABLE tag (
+    nome VARCHAR(255) NOT NULL, 
+    id_project INT NOT NULL,
+    descrizione TEXT , 
+    PRIMARY KEY (nome, id_project),
+    FOREIGN KEY (id_project) REFERENCES (project.id)
 );
 
 -- un tag può essere appartenente a più progetti => relazione tra tag e project 
-CREATE TABLE `db_app_techweb`.`tag_project` (
-    `id_project` INT NOT NULL, 
-    `nome_tag` INT NOT NULL,
-    PRIMARY KEY (`id_project`,`nome_tag`),
-    FOREIGN KEY (`id_project`) REFERENCES (`project`.`id`),
-    FOREIGN KEY (`nome_tag`) REFERENCES (`tag`.`nome`)
+CREATE TABLE tag_project (
+    id_project INT NOT NULL, 
+    nome_tag INT NOT NULL,
+    PRIMARY KEY (id_project,nome_tag),
+    FOREIGN KEY (id_project) REFERENCES (project.id),
+    FOREIGN KEY (nome_tag) REFERENCES (tag.nome)
 );
 
