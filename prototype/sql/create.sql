7920bb6a48da21a9994066f34c164584584a832f
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-28 17:37:10 +0100

    Aggiunta relazione progetto_utente al file .sql. Aggiunto il campo hash progetto all'interno della relazione progetto.

--- prototype/sql/create.sql
+++ prototype/sql/create.sql
@@ -5,6 +5,7 @@ CREATE TABLE progetto (
     id INT NOT NULL AUTO_INCREMENT , 
     nome VARCHAR(255) NOT NULL , 
     descrizione TEXT NOT NULL , 
+		link_condivisione VARCHAR(255) NULL UNIQUE,
     PRIMARY KEY (id));
 
 -- creazione tabella user
@@ -27,27 +28,28 @@ CREATE TABLE movimento (
     tag VARCHAR(255) NULL,
     PRIMARY KEY (id_progetto, data),
     FOREIGN KEY (id_progetto) REFERENCES progetto(id),
-    FOREIGN KEY (tag) REFERENCES tag(nome),
+    FOREIGN KEY (tag_id) REFERENCES tag(id), -- modifica
 );
 
 -- creazione della relazione tag
 CREATE TABLE tag (
+		id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL, 
     id_progetto INT NOT NULL,
     descrizione TEXT , 
+    PRIMARY KEY (id),
     PRIMARY KEY (nome, id_progetto),
+    FOREIGN KEY (id_progetto) REFERENCES progetto(id)
+);
+
+CREATE TABLE progetto_utente(
+    email VARCHAR(255) NOT NULL, 
+    id_progetto INT NOT NULL,
+    PRIMARY KEY (email, id_progetto),
+    FOREIGN KEY (email) REFERENCES user(email),
     FOREIGN KEY (id_progetto) REFERENCES progetto(id)
 );
 
 -- un tag può essere appartenente a più progetti e  
 -- più progetti possono averse lo stesso tag.
 -- => relazione molti a molti tra tag e progetto
-
-CREATE TABLE tag_progetto (
-    id_progetto INT NOT NULL, 
-    nome_tag VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_progetto,nome_tag),
-    FOREIGN KEY (id_progetto) REFERENCES progetto(id),
-    FOREIGN KEY (nome_tag) REFERENCES tag(nome)
-);
-
