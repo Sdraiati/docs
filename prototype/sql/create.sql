16b167f373997ee71a942f49a08cf9f8980b21fb
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-28 16:06:21 +0100

    Aggiunto campo tag alla tabella movimento.

--- prototype/sql/create.sql
+++ prototype/sql/create.sql
@@ -18,13 +18,16 @@ CREATE TABLE user (
 -- creazione della tabella movimento 
 -- spesa non sembra essere un nome adatto in quanto implica un
 -- importo sempre negativo
+-- il campo tag può essere nullo
 CREATE TABLE movimento (
     id_progetto INT NOT NULL , 
     data DATETIME NOT NULL , 
     importo FLOAT NOT NULL , 
     descrizione TEXT, 
+    tag VARCHAR(255) NULL,
     PRIMARY KEY (id_progetto, data),
     FOREIGN KEY (id_progetto) REFERENCES progetto(id),
+    FOREIGN KEY (tag) REFERENCES tag(nome),
 );
 
 -- creazione della relazione tag
@@ -36,7 +39,10 @@ CREATE TABLE tag (
     FOREIGN KEY (id_progetto) REFERENCES progetto(id)
 );
 
--- un tag può essere appartenente a più progetti => relazione tra tag e progetto 
+-- un tag può essere appartenente a più progetti e  
+-- più progetti possono averse lo stesso tag.
+-- => relazione molti a molti tra tag e progetto
+
 CREATE TABLE tag_progetto (
     id_progetto INT NOT NULL, 
     nome_tag VARCHAR(255) NOT NULL,
